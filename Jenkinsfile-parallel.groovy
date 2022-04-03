ccb69427f2a20db0463aca41ec9edb8dfc883e06
JengdiB <kajonkeati@gmail.com>
2022-04-03 12:46:35 +0700

    parallel pipeline with failFast option

--- Jenkinsfile-parallel.groovy
+++ Jenkinsfile-parallel.groovy
@@ -2,14 +2,18 @@ pipeline {
     agent any
     stages {
         stage('greeting-parallel') {
+            failFast true // to enforce parallel to aboart if one stage fail
             parallel {
                 stage('greeting-1') {
                     steps {
+                        sleep 10
                         echo 'Hello World-1!'
                     } //  steps
                 } // stage('greeting-1')
                 stage('greeting-2') {
                     steps {
+                        sleep 5
+                        error "error intentionally"
                         echo 'Hello World-2!'
                     } //  steps
                 } // stage('greeting-2')
